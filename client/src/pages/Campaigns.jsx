7f66f7ff7f9b9f1447a5bb45a6985774478a0733
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-19 17:59:26 +0530

    donate navigation provided in cam page

--- client/src/pages/Campaigns.jsx
+++ client/src/pages/Campaigns.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Campaigns = () => {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-900">
       <h2 className="text-3xl font-bold text-gray-900 mt-10 mb-6">Ongoing Campaigns</h2>
@@ -27,7 +29,7 @@ const Campaigns = () => {
             <h4 className="text-lg font-semibold text-gray-900 mb-3">{campaign.title}</h4>
             <p className="text-gray-700 mb-2">Funds Required: {campaign.funds}</p>
             <p className="text-gray-700 mb-2">Campaign ends in {campaign.days} Days</p>
-            <button className="bg-[#1882f2] hover:bg-[#1665c1] text-white px-5 py-2 rounded-lg transition-colors duration-300 mb-2">
+            <button onClick={() => (navigate('/donate'))} className="bg-[#1882f2] hover:bg-[#1665c1] text-white px-5 py-2 rounded-lg transition-colors duration-300 mb-2">
               DONATE NOW
             </button>
           </div>
